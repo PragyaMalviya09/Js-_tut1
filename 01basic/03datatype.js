dfdbb84107b10499e34ad4731ffd33e685e6a0e8
PragyaMalviya <144485984+PragyaMalviya09@users.noreply.github.com>
2023-09-10 10:52:18 +0000

    container config done

---
+++ 01basic/03datatype.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js  code as newer version
+
+//alert(4+4);
+console.log(3+3) // code readability is high
+  
+let name= "Pragya"
+let age= 27
+let isloggedIn =false
+
+// Number =>2 to the power of 52or may be 53
+//bigint is used for sharemarket and big no values
+//string =>"" it is string type that have in double or single quotes
+//boolean=> true/false it is having only two value
+//null=> null is type of standalone value
+//undefine => u are not define any value that is called undefine
+//symbol => the mainly implemented in reactjs when you make acomponent and find that component is unique that tyme we use symboll
+
+//object
+console.log(typeof age);
